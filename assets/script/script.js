0070ce9ba4d482f9c57721c25d2dfedead30b884
jonathansaandev <jonathansaandev@gmail.com>
2023-07-05 19:06:43 -0300

    menu

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,17 @@
 document.querySelector("#home").style.backgroundSize = "100% 100%";
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menu-item");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+  });
+});
