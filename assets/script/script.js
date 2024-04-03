e05ed66b8b1ffd696489302e40a9b75a653d4c96
jonathansaandev <jonathansaandev@gmail.com>
2024-04-03 00:10:35 -0300

    Delete images and update script.js and index.html

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,11 @@
-document.querySelector("#home").style.backgroundSize = "100% 100%";
-
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menu-item");
+
 const toTop = document.querySelector("#to-top");
+
+const popup = document.querySelector("#popup");
+const popupImg = document.querySelector("#popupImg");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -22,4 +24,19 @@ window.addEventListener("scroll", () => {
     return toTop.classList.add("active");
   }
   toTop.classList.remove("active");
-})
+});
+
+const showPopup = (imageUrl) => {
+  popupImg.src = imageUrl;
+  popup.style.display = "flex";
+  document.body.classList.add("popup-open");
+};
+
+popupImg.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
+  
+const hidePopup = () => {
+  popup.style.display = "none";
+  document.body.classList.remove("popup-open");
+};
